b90912e750cc8e1e3f535b5d69106d2b4830cef3
Fernando Cuatzo <cuatzo.fernando@gmail.com>
2022-12-21 15:18:21 -0600

    primer ejemplo googlemaps con react native markers y draggabl, permisos

--- App.js
+++ App.js
@@ -1,12 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import * as Location from 'expo-location'
+
+const environment = process.env.GOOGLE_MAPS_KEY
 
 export default function App() {
+  const [origin, setOrigin] = useState({
+    latitude: 33.640411,
+    longitude: -84.419853
+  })
+
+  const [destination, setDestionation] = useState({
+    latitude: 33.640411,
+    longitude: -84.419853
+  })
+
+  useEffect(() => {
+    getLocationPermission()
+  }, [])
+
+  async function getLocationPermission() {
+    let { status } = await Location.requestForegroundPermissionsAsync({})
+    if (status !== 'granted') alert('Permission Denied')
+
+    let location = await Location.getCurrentPositionAsync({})
+    const current = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    }
+
+    setOrigin(current)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+        style={styles.map}
+        initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04
+        }}
+       />
+       <Marker
+        draggable
+        coordinate={origin}
+        onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
+       />
+
+       <Marker
+        draggable
+        coordinate={destination}
+        onDragEnd={(direction) => setDestionation(direction.nativeEvent.coordinate)}
+       />
+
+       <MapViewDirections 
+          origin={origin}
+          destionation={destination}
+          apikey={environment}
+          strokeColor="black"
+          strokeWidth={8}
+       />
+       <Polyline  
+        coordinates={[origin, destination]}
+        strokeColor="pink"
+        strokeWidth={8}
+       />
     </View>
   );
 }
@@ -18,4 +82,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
 });
